e3746868a09b4f68faaf56690395c8aeff955d1e
Pierre Boileau <pboileau94@gmail.com>
2016-12-27 22:09:31 +0100

    Update harvester
    Harvester will now unload completely their load before they go back to
    miners for additioanal packages.

--- role.harvester.js
+++ role.harvester.js
@@ -5,22 +5,27 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep, miners) {
 
-        if(creep.carry.energy < creep.carryCapacity) {
-            
+        if(creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
+        }
+        else if(creep.carry.energy == creep.carryCapacity){
+            creep.memory.harvesting = false;
+        }
+
+        if(creep.memory.harvesting){
             var bestMiner = utilities.sortBestMinerForCreep(miners, creep)[0];
-            
             creep.say(bestMiner.name);
             if( bestMiner.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(bestMiner);
             }
         }
-        else {
+        else if (!creep.memory.harvesting){
             var sourceTargets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                             structure.energy < structure.energyCapacity;
                     }
-        })
+            })
             var targets = sourceTargets;
             if(sourceTargets.length == 0) {
 
